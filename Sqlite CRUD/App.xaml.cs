f2972784eac419adf9df28aa89829b85a1318a3d
kl_sys001 <kl_sys001@KL-SYS001PC>
2024-04-22 14:45:41 +0800

    Add project files.

---
+++ Sqlite CRUD/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace Sqlite_CRUD
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            DatabaseFacade facade = new DatabaseFacade(new DataContext());
+            facade.EnsureCreated();
+        }
+    }
+}
